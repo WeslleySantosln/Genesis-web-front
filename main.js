5e3d52a2ff8c26e202c950a760f710f91c9aab7b
WeslleySantosln <weslleys.santana299@gmail.com>
2022-12-07 14:07:39 -0300

    Atualização(clean input)

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ function addNewTask() {
     var text = document.getElementById("task_nome").value;
     var listItem = document.createElement("li");
     
-
     if(text.length === 0){
         alert('Digite uma tarefa!')
         return;
     }
+
     listItem.className = "list-item";
     const textElement = document.createTextNode(text);
     listItem.appendChild(textElement);
     list.appendChild(listItem);
+
+    document.getElementById("task_nome").value = "";    
 
 }
 
