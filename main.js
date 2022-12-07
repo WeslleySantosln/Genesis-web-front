672272be744e1ababbb541abba488d707d7aad52
WeslleySantosln <weslleys.santana299@gmail.com>
2022-12-07 13:01:47 -0300

    Atualização(Menu Responsivo)

--- main.js
+++ main.js
@@ -2,15 +2,24 @@ function addNewTask() {
     var list = document.getElementById("list");
     var text = document.getElementById("task_nome").value;
     var listItem = document.createElement("li");
-    listItem.className = "list-item";
+    
 
     if(text.length === 0){
         alert('Digite uma tarefa!')
         return;
     }
-
+    listItem.className = "list-item";
     const textElement = document.createTextNode(text);
     listItem.appendChild(textElement);
     list.appendChild(listItem);
+
+}
+
+function openNav(){
+  document.getElementById("myNav").style.width = '100%';  
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = '0'; 
 
 }
